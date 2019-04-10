43c602d48f01249a186493efd378807ba2b8df5e
root <root@tactfactory.com>
2019-04-10 20:08:01 +0200

    navigationDrawer + AppBar

--- src/main/java/fr/acos/androkado/views/activities/Main2Activity.java
+++ src/main/java/fr/acos/androkado/views/activities/Main2Activity.java
@@ -5,6 +5,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import fr.acos.androkado.R;
@@ -26,6 +28,13 @@ public class Main2Activity extends AppCompatActivity implements UtilisateurFragm
         UtilisateurFragment fragment = new UtilisateurFragment();
         fragmentTransaction.add(R.id.mainActivity2FragmentContainer, fragment);
         fragmentTransaction.commit();
+
+        ((Button) this.findViewById(R.id.button3)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Main2Activity.this.startActivity(new Intent(Main2Activity.this,Main5Activity.class));
+            }
+        });
     }
 
     @Override
