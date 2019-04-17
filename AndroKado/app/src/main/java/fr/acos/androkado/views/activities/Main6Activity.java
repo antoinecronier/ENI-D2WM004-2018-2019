f728025773f69de5fa4da9b7a1f75838a7f97b89
root <root@tactfactory.com>
2019-04-17 15:26:17 +0200

    webservice

--- AndroKado/app/src/main/java/fr/acos/androkado/views/activities/Main6Activity.java
+++ AndroKado/app/src/main/java/fr/acos/androkado/views/activities/Main6Activity.java
@@ -1,13 +1,19 @@
 package fr.acos.androkado.views.activities;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.JsonReader;
 import android.util.JsonWriter;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
@@ -15,8 +21,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import fr.acos.androkado.R;
 import fr.acos.androkado.entities.Utilisateur;
@@ -35,6 +48,7 @@ public class Main6Activity extends AppCompatActivity {
 
     private LinearLayout dataContent = null;
     private Gson gson = new Gson();
+    private EditText editText;
 
 
     @Override
@@ -43,6 +57,7 @@ public class Main6Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main6);
 
         this.dataContent = this.findViewById(R.id.viewContainer);
+        this.editText = this.findViewById(R.id.urltoFind);
     }
 
     public void btn6Clicked(View view) throws JSONException {
@@ -52,8 +67,21 @@ public class Main6Activity extends AppCompatActivity {
         this.populateView(json);
     }
 
-    public void btn5Clicked(View view) throws JSONException {
-        this.populateView2(new JSONObject(WEB_DATA));
+    public void btn5Clicked(View view) throws JSONException, ExecutionException, InterruptedException {
+
+        ConnectivityManager cm = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo ni = cm.getActiveNetworkInfo();
+
+        if (ni != null && ni.isConnected()){
+            /* API load */
+            String url = this.editText.getText().toString();
+
+            String result = new AccesDatas().execute(url).get();
+
+            this.populateView2(new JSONObject(result));
+        }else{
+            Toast.makeText(this,"Not connected to network",Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void btn4Clicked(View view) {
@@ -105,6 +133,42 @@ public class Main6Activity extends AppCompatActivity {
             linearLayout.addView(txtVData);
 
             dataContent.addView(linearLayout);
+        }
+    }
+
+    private class AccesDatas extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String urlView = strings[0];
+
+            HttpURLConnection httpURLConnection = null;
+
+            StringBuffer stringBuffer = new StringBuffer();
+
+            try {
+                URL url = new URL(urlView);
+
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                int charItem;
+
+                while ((charItem = inputStreamReader.read()) != -1){
+                    stringBuffer.append((char)charItem);
+                }
+
+                httpURLConnection.disconnect();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return stringBuffer.toString();
         }
     }
 }
